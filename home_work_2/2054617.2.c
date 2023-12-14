a7ed56191b90db7dc71ce6778eda10dbec8f1be6
Federico <federicodesisti@gmail.com>
2023-12-14 22:26:56 +0100

    ex2 homework2

---
+++ home_work_2/2054617.2.c
@@ -0,0 +1,299 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int ft_sqrt(int n)
+{
+    for(int i = 0; i <= n/2; i++)
+        if(i*i == n) return(i);
+    return(-69);
+}
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+void print_matrix(int n, int matrix[n*n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            printf("%d ", matrix[i*n + j]);
+        }
+        printf("\n");
+    }
+}
+
+
+int is_in_array(int k, int value, int *numbers)
+{
+    for(int i = 0; i < k - 1; i++)
+    {
+        if(value == numbers[i]) return 1;
+    }
+    return(0);
+}
+
+int search_in_array(int k, int value, int *numbers)
+{
+    for(int i = 0; i < k; i++)
+    {
+        if(value == numbers[i]) return i;
+    }
+    return(-1);
+}
+
+int search_in_column(int dim, int index_of_col_to_search, int number_to_find, int matrix[dim*dim])
+{
+    for(int i =1; i < dim ;i++)  
+    {
+        if(matrix[index_of_col_to_search + i*dim] == number_to_find)
+            return(index_of_col_to_search + i * dim);
+    }
+    return(-69);
+}
+int search_in_first_row(int dim, int matrix[dim*dim], int value)
+{
+    for(int i = 0; i < dim; i++)
+    {
+        if(matrix[i] == value)
+            return(i);
+    }
+    return(-69);
+}
+
+int check_if_number_already_seen(int dim, int matrix[dim*dim], int index, int index_array[dim*dim], int index_array_lenght)
+{
+    for(int i = 0; i < index_array_lenght; i++)
+    {
+        if(matrix[index_array[i]] == matrix[index])
+            return(1);
+    }
+    return(0);
+}
+
+
+
+void print_array(int n, int array[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+int    intersection(int dim, int ans_array[dim*dim], int index_array[dim*dim], int index_array_lenght)
+{
+    int k= 0;
+    int columns;
+    int row;
+    for(int i = 0; i < index_array_lenght; i++)
+    {
+        if (index_array[i] < dim)
+            ans_array[k++] = index_array[i];
+    }
+    columns = k;
+    for(int i = 0; i < index_array_lenght; i++)
+    {
+        if (index_array[i] % dim == 0)
+            ans_array[k++] = index_array[i];
+    }
+    row = k;
+    for(int i = 0; i < columns; i++)
+    {
+        for(int j = columns; j < row; j++)
+        {
+            ans_array[k++] = ans_array[i] + ans_array[j];
+        }
+    }
+    qsort(ans_array,k, sizeof(int), cmpfunc);
+    int j = 0;
+    for (int i = 0; i < k; i++)
+    {
+        if(search_in_array
+    (j, ans_array[i], ans_array) == -1)
+        {
+            ans_array[j++] = ans_array[i];
+        }
+    }
+    if(ft_sqrt(j) > dim/2) return(-1);
+    return (j);
+}
+
+/*
+6
+1 2 3 4 5 6
+2 1 4 3 6 5
+3 4 5 6 1 2
+4 3 6 5 2 1 
+5 6 1 2 3 4
+6 5 2 1 4 3
+*/
+
+void copyArray(int source[], int destination[], int size) {
+    for (int i = 0; i < size; i++) {
+        destination[i] = source[i];
+    }
+}
+
+int check_single_row(int row_to_search, int dim, int matrix[dim*dim], int ans_array[dim*dim], int ans_array_dim, int to_check[ft_sqrt(ans_array_dim)])
+{
+    int to_check_aux[ft_sqrt(ans_array_dim)];
+    int aux_index;
+    copyArray(to_check, to_check_aux, ft_sqrt(ans_array_dim));
+
+    for(int i = 0; i < ft_sqrt(ans_array_dim); i++)
+    {
+        aux_index = search_in_array(ft_sqrt(ans_array_dim), matrix[ans_array[row_to_search + i]],to_check_aux);
+        if(aux_index == -1) return(0);
+        to_check_aux[aux_index] = -1;
+    }
+    return(1);
+}
+
+int check_single_column(int column_to_search, int dim, int matrix[dim*dim], int ans_array[dim*dim], int ans_array_dim, int to_check[ft_sqrt(ans_array_dim)])
+{
+    int to_check_aux[ft_sqrt(ans_array_dim)];
+    int aux_index;
+    copyArray(to_check, to_check_aux, ft_sqrt(ans_array_dim));
+
+    for(int i = 0; i < ans_array_dim; i += ft_sqrt(ans_array_dim))
+    {
+        aux_index = search_in_array(ft_sqrt(ans_array_dim), matrix[ans_array[column_to_search + i]],to_check_aux);
+        if(aux_index == -1) return(0);
+        to_check_aux[aux_index] = -1;
+    }
+    return(1);
+}
+
+
+int check_rows_and_columns(int dim, int matrix[dim*dim], int ans_array[dim*dim], int ans_array_dim, int to_check[ft_sqrt(ans_array_dim)])
+{
+    for(int i = 0; i < ans_array_dim; i += ft_sqrt(ans_array_dim))
+    {
+        if(!check_single_row(i, dim, matrix, ans_array, ans_array_dim,to_check)) return(0);
+    }
+    for(int i = 0; i < ft_sqrt(ans_array_dim); i++)
+    {
+        if(!check_single_column(i, dim, matrix, ans_array, ans_array_dim,to_check)) return(0);
+    }
+    return(1);
+}
+
+int check_if_latin_square(int ans_array_dim, int dim, int ans_array[dim*dim], int matrix[dim*dim])
+{
+    int to_check[ft_sqrt(ans_array_dim)];
+    
+    for(int i = 0; i < ft_sqrt(ans_array_dim); i++)
+        to_check[i] = matrix[ans_array[i]];
+    // print_array(ft_sqrt(ans_array_dim), to_check);
+    if(check_rows_and_columns(dim, matrix, ans_array, ans_array_dim, to_check))
+    {
+        print_array(ans_array_dim, ans_array);  
+        return(1);
+    }
+    return(0);
+}
+
+int setup(int *index_of_col_to_search,int dim, int index_array[dim*dim], int *magic_number, int *k, int markers[dim], int matrix[dim*dim])
+{
+    *k = 2;
+    for(int i = 0; i < dim; i++)
+    {
+        if(markers[i] == 0)
+        {
+            *index_of_col_to_search = i;
+            markers[i] = 2;
+            index_array[1] = i;
+            *magic_number = matrix[i];
+            return(1);
+        }
+    }
+    return(0);
+}
+
+void    solve(int dim, int matrix[dim*dim])
+{
+    int number_to_find = matrix[0];
+    int magic_number = matrix[1];
+    int index_of_col_to_search = 1;
+    int index_of_number_to_find;
+    int index_of_first_column;
+    int index_array[dim*dim];
+    int k;
+    int ans_array[dim*dim];
+    int ans_array_dim;
+    int searched_col[dim];
+    int markers[dim];
+    
+    memset(markers, 0, dim);
+    markers[0] = 1;
+    index_array[0] = 0;
+    index_array[1] = 1;
+    k = 2;
+
+
+    while(1)
+    {
+        index_of_number_to_find = search_in_column(dim, index_of_col_to_search, number_to_find, matrix);
+        index_array[k++] = index_of_number_to_find;
+        index_of_first_column = index_of_number_to_find - index_of_col_to_search;
+        index_array[k++] = index_of_first_column;
+        if(check_if_number_already_seen(dim, matrix, index_of_first_column, index_array, k))
+        {
+            ans_array_dim = intersection(dim, ans_array, index_array, k);
+            if(ans_array_dim != -1)
+            {
+                if(check_if_latin_square(ans_array_dim, dim, ans_array, matrix))
+                {
+                    for(int i = 0; i < dim; i++) 
+                    {
+                        if(markers[i] == 2)
+                            markers[i] = 1;
+                    }
+                }
+                else
+                {
+                    for(int i = 0; i < dim; i++) 
+                    {
+                        if(markers[i] == 2)
+                            markers[i] = 0;
+                    }
+                }
+            printf("pene");
+            if(!setup(&index_of_col_to_search, dim, index_array, &magic_number, &k, markers, matrix)) return;
+            }
+        }
+        else
+        {
+            magic_number = matrix[index_of_first_column];
+            index_of_col_to_search = search_in_first_row(dim, matrix, magic_number);
+            markers[index_of_col_to_search] = 2;
+            index_array[k++] = index_of_col_to_search;
+        }
+    }
+}
+
+
+int main()
+{
+    int dim;
+    scanf("%d",&dim);
+    int input = dim*dim;
+    int matrix[dim*dim];
+    while(--input >= 0)
+    {
+        scanf("%d", &matrix[dim*dim - input - 1]);
+    }
+    printf("\n");
+
+    solve(dim, matrix);
+
+    // print_matrix(dim, matrix);
+    
+    // int test;
+    // scanf("%d", &test);
+    // printf("%d", matrix[test]);
+}
